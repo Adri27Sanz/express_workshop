e1e52104ff9196667b8f5fe7895f3d1ef61cb27e
Adriana Martinez <adrisanzm03@gmail.com>
2025-03-15 01:49:31 -0600

    Cambios sesion 5

--- index.js
+++ index.js
@@ -1,6 +1,61 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const { pokemon} = require ('./pokedex.json');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/*GET - Obtener recursos
+POST - amacenar recursos
+PATCH - modificar una parte del recurso
+PUT -  Modificar un recurso 
+DELETE - Borrar n recurso */
+
+app.get("/",(req /*Peticion que noos hace el cliente*/,res/*Respuesta que nosotros daremos, objeto que tien funciones para contestar peticiones*/, next)=>{
+    return res.status(200.).send("Bienvenido al Pokedex");
+});
+
+app.post("/pokemon",(req,res,next) =>{
+    return res.status(200).send(req.body);
+});
+
+app.get('/:pokemon',(req,res,next) => {
+    return res.status(200).send(pokemon);
+});
+
+
+
+app.get('/pokemon/:id([0-9]{1,3})',(req,res,next) => {
+    const id = req.params.id - 1; 
+    if (id >= 0 && id <= 150) {
+    return res.status(200).send(pokemon[req.params.id - 1]) 
+    }
+    return  res.status(404).send("Pokemon no encontrado")
+});
+
+
+app.get('/pokemon/:name([A-aZ-z]+)',(req, res, net) =>{
+
+    //Condicion ? valor si verdaadero : valor si falso
+    const name = req.params.name;
+
+    const pk = pokemon.filter((p) =>{
+      return  (p.name.toUpperCase () == name.toUpperCase()) && p;
+        
+    });
+
+
+    if (pk.length > 0) {
+        return res.status(200).send(pk)
+    }
+    return res.status(404).send("Pokémon no encontrado");
+});
+
+app.listen(process.env.PORT || 3000, () =>{
+    console.log ("Server is running....");
+
+});
 /*
 Verbos HTTP
 GET
@@ -9,13 +64,3 @@ PATCH-> Verbos destinados a la actualizacion de datos, SOLO MODIFICA UNOS ELEMEN
 PUT-> Modifica todos los elementos
 DELETE->Eliminar un registro
 */
-
-app.get("/",(req /*Peticion que noos hace el cliente*/,res/*Respuesta que nosotros daremos, objeto que tien funciones para contestar peticiones*/, next)=>{
-    res.status(200);
-    res.send("Bienvenido  :)");
-});
-app.listen(3000, () =>{
-    console.log ("Server is running..");
-
-});
-

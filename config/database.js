4739781bce3be9557623caca817bdfa6359b330f
Adriana Martinez <adrisanzm03@gmail.com>
2025-03-22 00:16:31 -0600

    Actividad 7

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     host: "localhost",  
     user: "root",
-    password: "",
+    password: "AdriMartinezSanz27",
     database: "pokemon"
 });
 
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const bodyParser = require('body-parser');
-const parser = require('morgan');
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
 const pokemon =require('./routes/pokemon');
-const morgan = require('morgan');
 
 
 app.use(morgan('dev'));
@@ -11,16 +9,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.get("/",(req /*Peticion que noos hace el cliente*/,res/*Respuesta que nosotros daremos, objeto que tien funciones para contestar peticiones*/, next)=>{
-    return res.status(200.).send("Bienvenido al Pokedex");
+
+app.get("/",(req,res, next)=>{
+    return res.status(200.).json({ code: 1, message: "Bienvenido al Pokedex"});
 });
 
 app.use("/pokemon",pokemon);
+
+app.use((req, res, next) => {
+    return res.status(404).json({code: 404, message: "URL no encontrada"});
+});
 
 app.listen(process.env.PORT || 3000, () =>{
     console.log ("Server is running....");
 
 });
+
+
+//(req /*Peticion que noos hace el cliente*/,res/*Respuesta que nosotros daremos, objeto que tien funciones para contestar peticiones*/, next
 /*
 Verbos HTTP
 GET

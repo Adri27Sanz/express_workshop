2050bcd614ef39194149fce782cb0e5622373956
Adriana Martinez <adrisanzm03@gmail.com>
2025-05-10 02:17:51 -0600

    Actividad 12

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const pokemon =require('./routes/pokemon');
 const user = require('./routes/user');
+const cors = require('./middleware-auth');
+const auth = require('./middleware/auth');
+const index = require('./middleware/index');
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -10,16 +13,16 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-app.get("/",(req,res, next)=>{
-    return res.status(200.).json({ code: 1, message: "Bienvenido al Pokedex"});
-});
+app.get("/",);
 
-app.use("/pokemon",pokemon);
+
 app.use("/user", user);
 
-app.use((req, res, next) => {
-    return res.status(404).json({code: 404, message: "URL no encontrada"});
-});
+app.use(auth);
+app.use("/pokemon",pokemon);
+
+
+app.use(notFound);
 
 app.listen(process.env.PORT || 3000, () =>{
     console.log ("Server is running....");

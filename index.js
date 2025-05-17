5e6b5e6823b506365b08dc37248d0614bc6750f6
Adriana Martinez <adrisanzm03@gmail.com>
2025-05-17 00:50:14 -0600

    Actividad 12

--- index.js
+++ index.js
@@ -14,21 +14,15 @@ const index = require('./middleware/index');
 const cors = require('./middleware/cors');
 
 
+app.use(cors);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
 app.get("/" , index);
-
-
 app.use("/user", user);
-
 app.use(auth);
 app.use("/pokemon",pokemon);
-
-
 app.use(notFound);
 
 app.listen(process.env.PORT || 3000, () =>{
